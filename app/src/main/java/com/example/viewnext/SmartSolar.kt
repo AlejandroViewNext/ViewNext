2dbf2109de5db8986cae213c493349a9a7d4597e
AlejandroViewNext <alejandro.morenog@viewnext.com>
2024-04-12 14:08:19 +0200

    terminado instalacion_fragment y energia_fragment

--- app/src/main/java/com/example/viewnext/SmartSolar.kt
+++ app/src/main/java/com/example/viewnext/SmartSolar.kt
@@ -1,15 +1,21 @@
 package com.example.viewnext
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.google.android.material.appbar.MaterialToolbar
+
 class SmartSolar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.smart_solar)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         // Obtener TabLayout y ViewPager
         val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
@@ -21,5 +27,11 @@ class SmartSolar : AppCompatActivity() {
 
         // Vincular ViewPager con TabLayout
         tabLayout.setupWithViewPager(viewPager)
+
+        // Configurar el clic en la Toolbar para redirigir a la actividad "Principal"
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
     }
 }
