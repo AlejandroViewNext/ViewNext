d59257c659d80f801939e3f49c267345ce0404bd
AlejandroViewNext <alejandro.morenog@viewnext.com>
2024-04-15 12:59:04 +0200

    Terminada navegación de la app y todas las vistas

--- app/src/main/java/com/example/viewnext/ForgotPassword.kt
+++ app/src/main/java/com/example/viewnext/ForgotPassword.kt
@@ -1,7 +1,10 @@
 package com.example.viewnext
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class ForgotPassword : AppCompatActivity() {
 
@@ -10,5 +13,13 @@ class ForgotPassword : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forgot_password)
 
+
+        val botonLogIn = findViewById<Button>(R.id.botonLogIn)
+
+        botonLogIn.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/viewnext/SingUp.kt
+++ app/src/main/java/com/example/viewnext/SingUp.kt
@@ -1,7 +1,10 @@
 package com.example.viewnext
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class SingUp : AppCompatActivity(){
 
@@ -9,5 +12,23 @@ class SingUp : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.singup)
 
+
+        val botonRegistrar = findViewById<Button>(R.id.botonReg)
+
+        botonRegistrar.setOnClickListener {
+            val intent = Intent(this, Principal::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+
+        val botonLogIn = findViewById<Button>(R.id.botonLogIn)
+        botonLogIn.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+
     }
+
+
 }
